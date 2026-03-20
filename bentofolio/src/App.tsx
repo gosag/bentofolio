2609e1ac55ef9f4b66f07a61fd25b655b4466672
Gosa Girma <gosagirma441@gmail.com>
2026-03-20 22:36:24 +0300

    feat: add initial components for the application structure

--- bentofolio/src/App.tsx
+++ bentofolio/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-black">
-      <h1 className="text-4xl font-bold text-blue-500">
+      <h1 className="text-4xl font-bold text-blue-500 animate-pulse">
         Tailwind is working 🚀
       </h1>
     </div>
