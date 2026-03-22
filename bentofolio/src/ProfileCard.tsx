aedab7d919205511dae8ccd1a4c534e24cefc0e7
Gosa Girma <gosagirma441@gmail.com>
2026-03-22 13:33:57 +0300

    feat: add lucide-react dependency and update ProfileCard component layout

--- bentofolio/src/ProfileCard.tsx
+++ bentofolio/src/ProfileCard.tsx
@@ -1,12 +1,15 @@
 import {Card,CardHeader,CardContent,CardFooter} from "./components/ui/card";
+import { Copy } from "lucide-react";
+
 export default function ProfileCard() {
     return(
+        <div className="flex flex-col">
         <Card className="max-w-[391px]">
                 <div className="flex flex-row items-center  gap-4 pl-4 ">
-                <img src="/profile.jpg" alt="Profile" className="rounded-sm w-16 h-16  object-cover" />
+                <img src="/profile.jpg" alt="Profile" className="rounded-sm w-20 h-20  object-cover" />
                 <div>
                     <p className="text-md w-fit px-2  bg-[#EEEEF1] text-[#71717A] rounded-lg">✌️ Welcome</p>
-                    <h2>Gosa Girma</h2>
+                    <h1 className="mt-4 text-lg font-semibold">Gosa Girma</h1>
                     <h4>I'm a Full stack Web Developer</h4>
                     
                 </div>
@@ -19,8 +22,13 @@ export default function ProfileCard() {
                 </p>
             </CardContent>
             <CardFooter className="border-t-0 flex items-center justify-center" >
-                <p className="text-center">gosagirma441@gmail.com</p>
+                
+                <div className="text-center flex justify-center items-center bg-[#EEEEF1] w-[343px] py-2 rounded-md text-[#303038]"><Copy size={20}/> <p className="ml-2"> gosagirma441@gmail.com</p></div>
             </CardFooter>
         </Card>
+        <Card className="max-w-[391px] mt-2">
+
+        </Card>
+        </div>
     )
 }
