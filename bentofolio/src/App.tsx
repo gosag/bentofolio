6a8da0fdc7568ffbdf1ee90a0942734d4a83aca7
Gosa Girma <gosagirma441@gmail.com>
2026-03-22 19:11:54 +0300

    Refactor App layout and add InfoGrid component
    - Removed App.css file and its associated styles.
    - Updated App.tsx to include InfoGrid component and adjusted layout styles.
    - Created InfoGrid component to display skills using Card UI.
    - Added SVG icons for various skills (TypeScript, React, Tailwind CSS, Node.js, JavaScript, HTML, Git, Express).
    - Cleaned up index.css by removing unused scrollbar styles.

--- bentofolio/src/App.tsx
+++ bentofolio/src/App.tsx
@@ -1,8 +1,10 @@
 import ProfileCard from "./ProfileCard";
+import InfoGrid from "./InfoGrid";
 export default function App() {
   return (
-    <div className=" min-h-screen ">
+    <div className=" min-h-screen flex gap-3 p-2">
       <ProfileCard />
+      <InfoGrid />
     </div>
   );
 }
