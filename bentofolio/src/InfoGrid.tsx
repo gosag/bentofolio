0f9483055ad95a0c6a79395dd9fe4350ddd10d04
Gosa Girma <gosagirma441@gmail.com>
2026-03-24 11:18:46 +0300

    feat: enhance MySkills component with additional skill icons and improve layout; add reusable Button component

--- bentofolio/src/InfoGrid.tsx
+++ bentofolio/src/InfoGrid.tsx
@@ -32,7 +32,14 @@ function MySkills(){
             <div className="flex min-w-32"><img src="/skills/html-svgrepo-com.svg" alt="HTML logo" title="HTML" className="w-10 h-10"/>HTML</div>
             <div className="flex min-w-32"><img src="/skills/git-icon-logo-svgrepo-com.svg" alt="Git Logo" title="Git" className="w-10 h-10"/>Git</div>
             </div>
-            <div className="border-l-2 border-l-gray-600 w-1/6">
+            <div className="border-l-2 border-l-gray-600 w-1/6 overflow-hidden flex">
+                <img src="/skills/typescript-svgrepo-com (1).svg" alt="Typescript Logo" title="Typescript" className="w-10 h-10"/>
+                <img src="/skills/react-svgrepo-com.svg" alt="React logo" title="React" className="w-10 h-10"/>
+                <img src="/skills/tailwind-css-svgrepo-com.svg"  alt="Tailwind logo" title="Tailwind CSS" className="w-10 h-10"/>
+                <img src="/skills/nodejs-icon-svgrepo-com.svg" alt="Node.js logo" title="Node.js" className="w-10 h-10"/>
+                <img src="/skills/expressjs-icon (1).svg" alt="Expess" title="Express Logo" className="w-10 h-10"/>
+                <img src="/skills/javascript-logo-svgrepo-com.svg" alt="JavaScript logo" title="JavaScript" className="w-10 h-10"/>
+                <img src="/skills/html-svgrepo-com.svg" alt="HTML logo" title="HTML" className="w-10 h-10"/>
                 <img src="/skills/git-icon-logo-svgrepo-com.svg" alt="Git Logo" title="Git" className="w-10 h-10"/>
             </div>
           </motion.div>
