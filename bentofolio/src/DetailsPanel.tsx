f14db4f4974512b6d1e30973d6ba2a218672c57d
Gosa Girma <gosagirma441@gmail.com>
2026-03-28 16:47:28 +0300

    feat:add resume to the app

--- bentofolio/src/DetailsPanel.tsx
+++ bentofolio/src/DetailsPanel.tsx
@@ -89,19 +89,22 @@ function Experiences() {
           </svg>
           Curriculum Vitae
         </div>
-        
+        <a href="/resume.pdf" download>
         <motion.button 
           whileHover={{ scale: 1.05, backgroundColor: "#f1f5f9" }}
           whileTap={{ scale: 0.95 }}
           className="p-2 rounded-full text-slate-400 hover:text-blue-600 transition-colors cursor-pointer "
           title="Download CV"
         >
+        
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
             <polyline points="7 10 12 15 17 10"></polyline>
             <line x1="12" y1="15" x2="12" y2="3"></line>
           </svg>
+          
         </motion.button>
+        </a>
       </div>
 
       <div className="p-6 overflow-y-auto flex-1 custom-scrollbar text-slate-600 dark:bg-zinc-900 dark:text-zinc-200">
